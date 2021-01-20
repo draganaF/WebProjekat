3463af94394490497251621d29c17e6507509258
Nikolina Tosic <nikolinatosic999@gmail.com>
2021-01-20 11:56:05 +0100

    Napravljen beans package sa apstraktnom klasom Korisnik

---
+++ Manifestacije/src/beans/Korisnik.java
@@ -0,0 +1,45 @@
+package beans;
+
+import java.util.Date;
+
+public abstract class Korisnik {
+	private String korisnickoIme,lozinka,ime,prezime,pol;
+	private Date datum;
+	public String getKorisnickoIme() {
+		return korisnickoIme;
+	}
+	public void setKorisnickoIme(String korisnickoIme) {
+		this.korisnickoIme = korisnickoIme;
+	}
+	public String getLozinka() {
+		return lozinka;
+	}
+	public void setLozinka(String lozinka) {
+		this.lozinka = lozinka;
+	}
+	public String getIme() {
+		return ime;
+	}
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+	public String getPrezime() {
+		return prezime;
+	}
+	public void setPrezime(String prezime) {
+		this.prezime = prezime;
+	}
+	public String getPol() {
+		return pol;
+	}
+	public void setPol(String pol) {
+		this.pol = pol;
+	}
+	public Date getDatum() {
+		return datum;
+	}
+	public void setDatum(Date datum) {
+		this.datum = datum;
+	}
+
+}
